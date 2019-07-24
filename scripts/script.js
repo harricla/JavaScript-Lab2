fcd433336a46f15158533273c110f0632294f69d
Claire Rose <clairemarieharrison@gmail.com>
2019-07-23 22:29:55 -0400

    final

--- scripts/script.js
+++ scripts/script.js
@@ -2,64 +2,58 @@
 const randomDamage = () => {
   return Math.floor(Math.random() * 10 + 1);
 };
-const damage = randomDamage();
-console.log(damage);
 
 // Arrow Function Step 2
-const randNum = () => {
-  return Math.floor(Math.random() * 2);
+const chooseOption = (Opt1, Opt2) => {
+  const randNum = Math.floor(Math.random() * 2);
+  return randNum === 0 ? Opt1 : Opt2;
 };
-function chooseOption(randNum) {
-  return (randNum = 0 ? "Opt1" : "Opt2");
-  console.log(chooseOption(true));
-  console.log(chooseOption(false));
-}
+
 // Function Expression
 const attackPlayer = function(health) {
-  return logHealth(health() - randomDamage);
+  return health - randomDamage();
 };
 
 const logHealth = (player, health) => {
-  console.log("player health: ${health}");
+  console.log(`${player} health: ${health}`);
 };
+
 // // Arrow Function
 
 const logDeath = (winner, loser) => {
-  console.log("winner defeated loser");
+  console.log(`${winner} defeated ${loser}`);
 };
+
 // true or false
 const isDead = health => {
   return health <= 0;
 };
 
-// // Function Declaration
+// // // Function Declaration
 function fight() {
-    const player1 = "Mitch" ;
-    const player2 = "Adam" ;
-    const player1Health: 100;
-    const player2Health: 100;
-    while (true) {
-        let attacker = chooseOption(player1, player2) {
-        if (attacker === player1){
-            player2Health = attacker;
-            logDeath(player1, player2);
-            break;
-        } else {
-            player1Health === attackPlayer(player1Health){
-                logHealth(player1, player1Health); {
-                    if (isDead(player1Health));
-                    logDeath(player2, player1);
-                    break;
-                }
-            }
+  const player1 = "Mitch";
+  const player2 = "Adam";
+  let player1Health = 100;
+  let player2Health = 100;
+  while (true) {
+    let attacker = chooseOption(player1, player2);
+    if (attacker === player1) {
+      player2Health = attackPlayer(player2Health);
+      logHealth(player2, player2Health);
 
-        }
-      return fight;
-      
-}
- {
+      if (isDead(player2Health) === true) {
+        logDeath(player1, player2);
+        break;
+      }
+    } else {
+      player1Health = attackPlayer(player1Health);
+      logHealth(player1, player1Health);
 
+      if (isDead(player1Health) === true) {
+        logDeath(player2, player1);
+        break;
+      }
     }
-        console.log(player1) ;
-        console.log(player2) ;
+  }
 }
+fight();
